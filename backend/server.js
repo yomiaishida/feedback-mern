7a51ab54a2eaf788e01ad0384ff2e4991bdd2674
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-13 15:04:33 +0100

    Finalling getting req.body through epress.json()

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import feedbacks from "./data/feedbacks.js";
+// import feedbacks from "./data/feedbacks.js";
+import Feedback from "./models/feedbackModel.js";
 import colors from "colors";
 
 dotenv.config();
@@ -9,24 +10,47 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/feedback", (req, res) => {
-  res.json(feedbacks);
-});
+app.get("/api/feedback", async (req, res) => {
+  // res.json(Feedback);
 
-app.get("/api/feedback/:id", (req, res) => {
-  const feedback = feedbacks.find((f) => f._id === req.params.id);
+  const feedback = await Feedback.find();
+  console.log(feedback);
   res.json(feedback);
 });
 
-app.delete("/api/feedback/:id", (req, res) => {
-  const feedback = feedbacks.findById(req.params.id);
+app.post("/api/create", async (req, res) => {
+  // res.send("API is running...");
+  // console.log(req.body);
+  const { rating, text } = req.body;
+
+  try {
+    const feed = await Feedback.create({ rating, text });
+    res.status(200).json(feed);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/api/feedback/:id", (req, res) => {
+  const feedback = Feedback.find((f) => f._id === req.params.id);
   if (feedback) {
-    feedback.remove();
+    res.json(feedback);
+  } else {
+    res.status(404);
+    throw new Error("Feedback not found");
+  }
+});
+
+app.delete("/api/feedback/:id", async (req, res) => {
+  const feedback = Feedback.findById(req.params.id);
+  if (feedback) {
+    await feedback.remove();
     res.json({ message: "Feedback removed" });
   } else {
     res.status(404);
